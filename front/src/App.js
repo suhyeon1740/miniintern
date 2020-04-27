6cbf9d03f39562479dd057ee571b979ca2892d01
Elly (박수현) <elly@treenod.com>
2020-04-27 13:55:36 +0900

    라우터 구현

--- front/src/App.js
+++ front/src/App.js
@@ -2,15 +2,21 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
-// import About from './pages/About/About'
-import Projects from './pages/projects/Projects'
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Projects from './pages/Projects/Projects'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Projects />
-      <Footer />
+    <div className="App">      
+      <Router>
+        <Header />
+        <Route exact path="/" component={Home} /> 
+        <Route path="/projects" component={Projects} />
+        <Route path="/about" component={About} />
+        <Footer />
+      </Router>      
     </div>
   );
 }
